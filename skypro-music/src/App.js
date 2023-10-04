0841f8a1bab29315191b0a75d314b3bb643f1449
Ulyana0406 <yliana0406@mail.ru>
2023-10-04 11:57:10 +0300

    HM1

---
+++ skypro-music/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="App-link">Здесь скоро будет Skypro.Music</p>
+    </div>
+  );
+}
+
+export default App;
