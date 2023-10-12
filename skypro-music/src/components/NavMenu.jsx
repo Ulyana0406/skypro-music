b406111b703b3668a8037c1f3cdd39c96736ffb0
Ulyana0406 <yliana0406@mail.ru>
2023-10-12 18:38:38 +0300

    NavMenu is done

--- skypro-music/src/components/NavMenu.jsx
+++ skypro-music/src/components/NavMenu.jsx
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import "../App.css";
 import NavMyPlaylist from "./MyPlaylist";
 import SighInButton from "./SingIn";
 import NavMain from "./Main";
+import React from "react";
 function NavMenu() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
         <img className="logo__image" src="img/logo.png" alt="logo" />
       </div>
+
       <div className="nav__burger burger">
-        <span className="burger__line" />
-        <span className="burger__line" />
-        <span className="burger__line" />
+        <div
+          onClick={() => {
+            setVisible(!visible);
+          }}
+          role="button"
+        >
+          <img className="click_button" src="img/burger.png" alt="logo" />
+        </div>
       </div>
       <div className="nav__menu menu">
-        <ul className="menu__list">
-          <NavMain />
-          <NavMyPlaylist />
-          <SighInButton />
-        </ul>
+        {visible && (
+          <ul className="menu__list">
+            <NavMain />
+            <NavMyPlaylist />
+            <SighInButton />
+          </ul>
+        )}
       </div>
     </nav>
   );
