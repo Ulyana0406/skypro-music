f30b95ceb2b61aef283297c4815671e45034851a
Ulyana0406 <yliana0406@mail.ru>
2023-10-15 20:28:30 +0300

    Готово ^-^

--- skypro-music/src/App.jsx
+++ skypro-music/src/App.jsx
@@ -4,14 +4,93 @@ import SideBar from "./components/SideBar";
 import MainCenterblok from "./components/TrackList";
 import NavMenu from "./components/NavMenu";
 import AudioPlayer from "./components/AudioPlayer";
-
+import Search from "./components/Search";
+import Filter from "./components/Filter";
 function App() {
   return (
     <div className="wrapper">
       <div className="container">
         <main className="main">
           <NavMenu />
-          <MainCenterblok />
+          <div className="main__centerblock centerblock">
+            <Search />
+            <h2 className="centerblock__h2">Треки</h2>
+            <Filter />
+            <div className="centerblock__content">
+              <div className="content__title playlist-title">
+                <div className="playlist-title__col col01">Трек</div>
+                <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
+                <div className="playlist-title__col col03">АЛЬБОМ</div>
+                <div className="playlist-title__col col04">
+                  <svg className="playlist-title__svg" alt="time">
+                    <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="content__playlist playlist">
+              <MainCenterblok
+                trackName="Guilt"
+                trackAuthor="Nero"
+                album="Weloome Reality"
+                trackTime="4.44"
+              />
+              <MainCenterblok
+                trackName="Elektro"
+                trackAuthor="Dynoro, Outwork, Mr. Gee"
+                album="Elektro"
+                trackTime="2.22"
+              />
+              <MainCenterblok
+                trackName="I’m Fire"
+                trackAuthor="Ali Bakgor"
+                album="I’m Fire"
+                trackTime="2.22"
+              />
+              <MainCenterblok
+                trackName="Non Stop"
+                trackAuthor="Стоункат, Psychopath"
+                album="Weloome Reality"
+                trackTime="4.12"
+              />
+              <MainCenterblok
+                trackName="Run Run"
+                trackAuthor="Jaded, Will Clarke, AR/CO"
+                album="Run Run"
+                trackTime="2.54"
+              />
+              <MainCenterblok
+                trackName="Eyes on Fire"
+                trackAuthor="Blue Foundation, Zeds Dead"
+                album="Eyes on Fire"
+                trackTime="5.20"
+              />
+              <MainCenterblok
+                trackName="Mucho Bien"
+                trackAuthor="HYBIT, Mr. Black, Offer Nissim, Hi Profile"
+                album="Mucho Bien"
+                trackTime="3.41"
+              />
+              <MainCenterblok
+                trackName="Knives n Cherries"
+                trackAuthor="minthaze"
+                album="Captivating"
+                trackTime="1.48"
+              />
+              <MainCenterblok
+                trackName="How Deep Is Your Love"
+                trackAuthor="Calvin Harris, Disciples"
+                album="How Deep Is Your Love"
+                trackTime="3.32"
+              />
+              <MainCenterblok
+                trackName="Morena"
+                trackAuthor="Tom Boxer"
+                album="Soundz Made in Romania"
+                trackTime="3.36"
+              />
+            </div>
+          </div>
           <SideBar />
         </main>
         <AudioPlayer />
--- skypro-music/src/components/SideBar.jsx
+++ skypro-music/src/components/SideBar.jsx
@@ -1,6 +1,11 @@
 import "../App.css";
-
+import React, { useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 function SideBar() {
+  const [isLoading, setLoading] = useState(false);
+  setTimeout(() => {
+    setLoading(true);
+  }, 4000);
   return (
     <div className="main__sidebar sidebar">
       <div className="sidebar__personal">
@@ -14,31 +19,64 @@ function SideBar() {
       <div className="sidebar__block">
         <div className="sidebar__list">
           <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist01.png"
-                alt="day's playlist"
-              />
-            </a>
+            <SkeletonTheme
+              baseColor="#313131"
+              highlightColor="#fff"
+              height={150}
+              width={250}
+            >
+              {isLoading ? (
+                <a className="sidebar__link" href="#">
+                  <img
+                    className="sidebar__img"
+                    src="img/playlist01.png"
+                    alt="day's playlist"
+                  />
+                </a>
+              ) : (
+                <Skeleton />
+              )}
+            </SkeletonTheme>
           </div>
           <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist02.png"
-                alt="day's playlist"
-              />
-            </a>
+            <SkeletonTheme
+              baseColor="#313131"
+              highlightColor="#fff"
+              height={150}
+              width={250}
+            >
+              {isLoading ? (
+                <a className="sidebar__link" href="#">
+                  <img
+                    className="sidebar__img"
+                    src="img/playlist02.png"
+                    alt="day's playlist"
+                  />
+                </a>
+              ) : (
+                <Skeleton />
+              )}
+            </SkeletonTheme>
           </div>
           <div className="sidebar__item">
-            <a className="sidebar__link" href="#">
-              <img
-                className="sidebar__img"
-                src="img/playlist03.png"
-                alt="day's playlist"
-              />
-            </a>
+            <SkeletonTheme
+              baseColor="#313131"
+              highlightColor="#fff"
+              height={150}
+              width={250}
+            >
+              {isLoading ? (
+                <a className="sidebar__link" href="#">
+                  <img
+                    className="sidebar__img"
+                    src="img/playlist03.png"
+                    alt="day's playlist"
+                  />
+                </a>
+              ) : (
+                <Skeleton />
+              )}
+            </SkeletonTheme>
           </div>
         </div>
       </div>
