a7a6b8ff1a5ed06575f059d39ea5b93da41202c8
Ulyana0406 <yliana0406@mail.ru>
2024-01-09 21:03:05 +0300

    Доработан фильтр

--- skypro-music/src/pages/Main.jsx
+++ skypro-music/src/pages/Main.jsx
@@ -9,38 +9,37 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setAlltracks, tracksRedux } from "../store/slices/trackSlice";
 import { setAccess, setRefresh, setUserData } from "../store/slices/authSlice";
-
-
+import { Filter } from "../components/Filter/Filter";
 export const Main = () => {
   const { loading, tracks, tracksError, setPlaylist } = useContext(Context);
   const songs = useSelector((state) => state.playlist.tracks);
   const favorites = useSelector((state) => state.playlist.favorites);
   const dispatch = useDispatch();
-  const filteredSongs = useSelector((state) => state.playlist.FiltersPlaylist.filterTracksArr);
+  const filteredSongs = useSelector(
+    (state) => state.playlist.FiltersPlaylist.filterTracksArr
+  );
 
   useEffect(() => {
-    if (filteredSongs.length === 0){
-      dispatch(tracksRedux(tracks))
-      setPlaylist(tracks)
-    }
-    else {
-      dispatch(tracksRedux(filteredSongs))
-      setPlaylist(filteredSongs)
+    if (filteredSongs.length === 0) {
+      dispatch(tracksRedux(tracks));
+      setPlaylist(tracks);
+    } else {
+      dispatch(tracksRedux(filteredSongs));
+      setPlaylist(filteredSongs);
     }
     // eslint-disable-next-line
-  }, [filteredSongs])
+  }, [filteredSongs]);
 
   useEffect(() => {
-    dispatch(setAlltracks(tracks))
+    dispatch(setAlltracks(tracks));
     // eslint-disable-next-line
-  }, [songs])
+  }, [songs]);
 
   useEffect(() => {
     if (tracks.length) {
-      dispatch(tracksRedux(tracks))
-      setPlaylist(songs)
-
-    };
+      dispatch(tracksRedux(tracks));
+      setPlaylist(songs);
+    }
     // eslint-disable-next-line
   }, [dispatch, tracks, favorites]);
 
@@ -56,14 +55,14 @@ export const Main = () => {
 
   return (
     <>
-        <GlobalStyle />
-        <S.centoblockTittle>Треки</S.centoblockTittle>
-       
-        {tracksError ? (
-          <p>Не удалось загрузить плейлист, попробуйте позже</p>
-        ) : (
-          <TreckList tracks={songs} loading={loading} />
-        )}
-  </>
+      <GlobalStyle />
+      <S.centoblockTittle>Треки</S.centoblockTittle>
+      <Filter />
+      {tracksError ? (
+        <p>Не удалось загрузить плейлист, попробуйте позже</p>
+      ) : (
+        <TreckList tracks={songs} loading={loading} />
+      )}
+    </>
   );
 };
