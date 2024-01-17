20c8c48a040c4a7c61161216f084b61b639eeeac
Ulyana0406 <yliana0406@mail.ru>
2024-01-17 13:12:44 +0300

    Исправлены ошибки

--- skypro-music/src/components/Filter/Filter.jsx
+++ skypro-music/src/components/Filter/Filter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import * as S from "./Filter.styles"
-import uniq from 'lodash.uniq'
-import { useDispatch, useSelector } from 'react-redux'
+import * as S from "./Filter.styles";
+import _ from "lodash";
+import { useDispatch, useSelector } from "react-redux";
 import { TracksFilterCategory } from "./TrackFilterCategory";
 import { setFiltersPlaylist } from "../../store/slices/trackSlice";
 import { filtersPlaylistSelector } from "../../store/selectors";
@@ -11,10 +11,10 @@ import Context from "../../context";
 const { useState } = React;
 
 export const Filter = () => {
-  const dispatch = useDispatch()
-  const [activeCategoryFilter, setActiveCategoryFilter] = useState('')
+  const dispatch = useDispatch();
+  const [activeCategoryFilter, setActiveCategoryFilter] = useState("");
 
-  const selectedFiltersPlaylist = useSelector(filtersPlaylistSelector)
+  const selectedFiltersPlaylist = useSelector(filtersPlaylistSelector);
   const { tracks } = useContext(Context);
 
   return (
@@ -24,43 +24,43 @@ export const Filter = () => {
         <TracksFilterCategory
           nameCategory="исполнителю"
           numberSelectedValues={selectedFiltersPlaylist?.authors.length}
-          content={uniq(
-            tracks?.map((track) => track?.author)
-          ).map((author) => (
-            <S.FilterItem
-              key={author}
-              onClick={() => {
-                dispatch(setFiltersPlaylist({ authors: author }))
-              }}
-              $isSelected={selectedFiltersPlaylist?.authors.includes(author)}
-            >
-              {author}
-            </S.FilterItem>
-          ))}
+          content={_.uniq(tracks?.map((track) => track?.author)).map(
+            (author) => (
+              <S.FilterItem
+                key={author}
+                onClick={() => {
+                  dispatch(setFiltersPlaylist({ authors: author }));
+                }}
+                $isSelected={selectedFiltersPlaylist?.authors.includes(author)}
+              >
+                {author}
+              </S.FilterItem>
+            )
+          )}
           isActiveCategory={activeCategoryFilter}
           setActiveCategory={setActiveCategoryFilter}
         />
-        {(
+        {
           <TracksFilterCategory
             nameCategory="жанру"
             isActiveCategory={activeCategoryFilter}
             setActiveCategory={setActiveCategoryFilter}
             numberSelectedValues={selectedFiltersPlaylist?.genres.length}
-            content={uniq(
-              tracks?.map((track) => track.genre)
-            ).map((genre) => (
-              <S.FilterItem
-                key={genre}
-                onClick={() => {
-                  dispatch(setFiltersPlaylist({ genres: genre }))
-                }}
-                $isSelected={selectedFiltersPlaylist?.genres.includes(genre)}
-              >
-                {genre}
-              </S.FilterItem>
-            ))}
+            content={_.uniq(tracks?.map((track) => track.genre)).map(
+              (genre) => (
+                <S.FilterItem
+                  key={genre}
+                  onClick={() => {
+                    dispatch(setFiltersPlaylist({ genres: genre }));
+                  }}
+                  $isSelected={selectedFiltersPlaylist?.genres.includes(genre)}
+                >
+                  {genre}
+                </S.FilterItem>
+              )
+            )}
           />
-        )}
+        }
       </S.filterDiv>
       <S.filterDiv>
         <S.filterTitle>Сортировка:</S.filterTitle>
@@ -69,14 +69,14 @@ export const Filter = () => {
           isActiveCategory={activeCategoryFilter}
           setActiveCategory={setActiveCategoryFilter}
           numberSelectedValues={
-            selectedFiltersPlaylist?.sort === 'По умолчанию' ? 0 : 1
+            selectedFiltersPlaylist?.sort === "По умолчанию" ? 0 : 1
           }
-          content={['По умолчанию', 'Сначала новые', 'Сначала старые'].map(
+          content={["По умолчанию", "Сначала новые", "Сначала старые"].map(
             (item) => (
               <S.FilterItem
                 key={item}
                 onClick={() => {
-                  dispatch(setFiltersPlaylist({ sort: item }))
+                  dispatch(setFiltersPlaylist({ sort: item }));
                 }}
                 $isSelected={selectedFiltersPlaylist?.sort.includes(item)}
               >
@@ -87,5 +87,5 @@ export const Filter = () => {
         />
       </S.filterDiv>
     </S.CenterBlockFilter>
-  )
-}
+  );
+};
